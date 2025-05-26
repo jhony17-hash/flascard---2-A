8999c1e0ff27e32fcc23a865f19969bb5b378750
jhony17-hash <jhony.castello@escola.pr.gov.br>
2025-05-26 10:24:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
+function criaCARTAO(categoria, pergunta, resposta){
+  let container=document. getElementByld('container')
+  let cartao = document.createElement('article')
+  cartao.className ='cartao'  
 
+cartao.innerHTML =`
+<div class="cartao__conteudo">
+<h3>${categoria}</h3>
+<div class="cartao__conteudo__pergonta">
+<p>${pergunta}</p>
+</div>
+<div class="cartao__conteudo__resposta">
+<p>${resposta</p>
+     </div>
+     </div>
+
+     let respostaEstaVisivel = false
+
+     function vira Cartao(){
+     respostaEstaVisivel = !respostaEstaVisivel
+     cartao.classList.toggle(',respostaEstaVisivel)
+     {
+     cartao.addEventListener(', viraCartao)
+
+     container.appendChild(cartao)
+
+     }
